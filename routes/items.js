eedb330082e65dbf92e5cd242db0efbef48e1095
MonicaGrages <mgrages1@gmail.com>
2017-05-25 16:59:51 -0400

    set up basic food item router with index route

--- routes/items.js
+++ routes/items.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource for users index');
+  res.send('respond with a resource for food items index');
 });
 
 module.exports = router;
