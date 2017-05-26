677eeff3fce5518d901f2d8512f5ef4c18f8007d
MonicaGrages <mgrages1@gmail.com>
2017-05-26 12:47:26 -0400

    SEEDS WORKING ON HEROKU

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ var FoodItem = require('../models/item.js')
 
 /* Index route for showing all users */
 router.get('/', function(request, response) {
-    User.find({})
+  User.find({})
     .exec(function (error, usersList) {
       if(error) {
         console.log('Error trying to get usersList: '+error);
