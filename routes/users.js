53a32494e562c31262cbe068444fed0c7daf59b8
MonicaGrages <mgrages1@gmail.com>
2017-05-26 08:44:33 -0400

    just starting to set up db

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource for users index');
+  res.send('show all the users here');
 });
+
+//show the new user form
+router.get('/new', function (request, response) {
+  response.send('show the new user form here');
+});
+
 
 module.exports = router;
