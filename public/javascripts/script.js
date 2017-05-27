b9bde82c4aa740631cdbc49f11171257cc2ce490
MonicaGrages <mgrages1@gmail.com>
2017-05-27 16:11:23 -0400

    added expiration date alert

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,2 +1,8 @@
-console.log('javascript was here');
+var currentDate = new Date();
 
+var expirationDate = new Date($('.expirationDate').html());
+
+if (currentDate > expirationDate) {
+  $('.food_info').prepend('<p class="expiration_message">This food has expired</p>');
+  $('.expirationDate').addClass('expired_date');
+}
