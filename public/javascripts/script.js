6c23d9c47ba89f801aac5368c715e5137e83317e
MonicaGrages <mgrages1@gmail.com>
2017-05-27 21:28:38 -0400

    changed food group form input to radio buttons instead of text

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,23 +1,26 @@
+$(function () {
+  //if this is a food item show page (has expiration date div on it)
+  if ($('.expirationDate').length > 0) {
+    //if there is no expiration date entered
+    if($('.expirationDate').html().length === 0) {
+      //just empty the whole expiration date line
+      $('.expiration_line').empty();
+      //if there is an expiration date listed
+    } else if ($('.expirationDate').html().length > 0) {
+      var currentDate = new Date();
+      //grab food expiration date from DOM and turn back into date object
+      var expirationDate = new Date($('.expirationDate').html());
 
-//if this is a page with an expiration date div on it
-if ($('.expirationDate').length > 0) {
-  //if there is no expiration date entered
-  if($('.expirationDate').html().length === 0) {
-    //just empty the whole expiration date line
-    $('.expiration_line').empty();
-    //if there is an expiration date listed
-  } else if ($('.expirationDate').html().length > 0) {
-    var currentDate = new Date();
-    //grab food expiration date from DOM
-    var expirationDate = new Date($('.expirationDate').html());
-
-    //check whether expiration date has passed
-    if (currentDate > expirationDate) {
-      $('.food_info').prepend('<p class="expiration_message">This food has expired</p>');
-      $('.expirationDate').addClass('expired_date');
-
+      //check whether expiration date has passed
+      if (currentDate > expirationDate) {
+        //add food is expired message to item show page
+        $('.food_info').prepend('<p class="expiration_message">This food has expired</p>');
+        $('.expirationDate').addClass('expired_date');
+      }
+       //change date format instead of ISO
+      $('.expirationDate').html(expirationDate.toLocaleDateString());
     }
-     //change date format instead of ISO
-    $('.expirationDate').html(expirationDate.toLocaleDateString());
   }
-}
+});
+
+
