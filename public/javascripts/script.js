602203cfe8c6315118bdd5eeebf1098108df7301
MonicaGrages <mgrages1@gmail.com>
2017-05-27 20:39:42 -0400

    finished expiration date check

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,8 +1,22 @@
-var currentDate = new Date();
 
-var expirationDate = new Date($('.expirationDate').html());
+//if this is a page with an expiration date div on it
+if ($('.expirationDate').length > 0) {
+  //if there is no expiration date entered
+  if($('.expirationDate').html().length === 0) {
+    //just empty the whole expiration date line
+    $('.expiration_line').empty();
+    //if there is an expiration date listed
+  } else if ($('.expirationDate').html().length > 0) {
+    var currentDate = new Date();
+    //grab food expiration date from DOM
+    var expirationDate = new Date($('.expirationDate').html());
 
-if (currentDate > expirationDate) {
-  $('.food_info').prepend('<p class="expiration_message">This food has expired</p>');
-  $('.expirationDate').addClass('expired_date');
+    //check whether expiration date has passed
+    if (currentDate > expirationDate) {
+      $('.food_info').prepend('<p class="expiration_message">This food has expired</p>');
+      $('.expirationDate').addClass('expired_date');
+      //and pretty print date instead of ISO format
+      $('.expirationDate').html(expirationDate.toLocaleDateString());
+    }
+  }
 }
